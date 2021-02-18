a622530b07c738d0859be592a525445abc5b2ea8
asma20 <jbeli.asma20@gmail.com>
2021-02-18 14:26:03 +0100

    incémentation

---
+++ Exercice3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercice3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1, number2;
+            Console.WriteLine("Entrer le 1 ére nombre :");
+            number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Entrer le 2éme nombre :");
+            number2 = Convert.ToInt32(Console.ReadLine());
+            float s = number1 + 33;
+            number2 ++;
+            float division = s/ number2;
+            Console.WriteLine("Somme "+s);
+            Console.WriteLine("L'incrémentation" + number2);
+            Console.WriteLine("la resultat de division est "+division);
+
+
+
+        }
+    }
+}
